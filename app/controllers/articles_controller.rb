f82d2790963a9e9a9ce02d60420a4f1ce8e63c40
Urs Zysset <UrsZysset@gmail.com>
2014-09-19 17:20:28 +0200

    tutorial done!

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,7 @@
 class ArticlesController < ApplicationController
 
+	http_basic_authenticate_with name: "dhh", password: "secret", except: [:index, :show]
+	
 	def new
 		@article = Article.new
 	end
@@ -24,6 +26,23 @@ class ArticlesController < ApplicationController
 	
 	def edit
 		@article = Article.find(params[:id])
+	end
+	
+	def update
+		@article = Article.find(params[:id])
+ 
+		if @article.update(article_params)
+			redirect_to @article
+		else
+			render 'edit'
+		end
+	end
+	
+	def destroy
+		@article = Article.find(params[:id])
+		@article.destroy
+ 
+		redirect_to articles_path
 	end
 	
 	private
